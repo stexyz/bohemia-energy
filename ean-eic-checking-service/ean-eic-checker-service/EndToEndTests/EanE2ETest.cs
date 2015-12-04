40fce84b1e37874989c0563dfb911c94356472a0
Stefan Pacinda <pacinda@seznam.cz>
2015-12-04 02:58:40 +0100

    E2E test filled with static data.

--- ean-eic-checking-service/ean-eic-checker-service/EndToEndTests/EanE2ETest.cs
+++ ean-eic-checking-service/ean-eic-checker-service/EndToEndTests/EanE2ETest.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using EndToEndTests.HttpClient;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ean_eic_checker_service.Models;
 using ean_eic_checker_service.Tests.HttpClient;
 
 namespace EndToEndTests {
@@ -18,7 +19,8 @@ namespace EndToEndTests {
             httpClient = new EanEicCheckerHttpClient(@"http://be-ean-eic-validator.azurewebsites.net/api/EanEicCheck");
         }
 
-        [TestMethod]
+        //TODO SP: fix the linking of the csv file
+        //[TestMethod]
         public void TestEanCodes() {
             using (StreamReader sr = new StreamReader(SimpleTestPath)) {
                 string currentLine;
@@ -28,6 +30,40 @@ namespace EndToEndTests {
                 }
             }
 //            CheckResult res = httpClient.Post(_validEan);
+        }
+
+
+        private readonly EanEicCode _validEan = new EanEicCode("859182400123456789");
+        private readonly EanEicCode _nonDigitEan = new EanEicCode("859x82D00&*(4567▼˘");
+        private readonly EanEicCode _invalidSumcheckEan = new EanEicCode("859182400123456784");
+        private readonly EanEicCode _tooLongEan = new EanEicCode("859182400123456789654465");
+        private readonly EanEicCode _tooShortEan = new EanEicCode("859182400129");
+        private readonly EanEicCode _invalidPrefix = new EanEicCode("X9ZG600Z0424987D");
+
+
+        [TestMethod]
+        public void TestEanCodesWithInlineParams()
+        {
+            CheckResult res = httpClient.Post(_validEan);
+            Assert.AreEqual(CheckResultCode.EanOk, res.ResultCode);
+
+            res = httpClient.Post(_invalidPrefix);
+            Assert.AreEqual(CheckResultCode.CodePrefixInvalid, res.ResultCode);
+
+            res = httpClient.Post(_tooLongEan);
+            Assert.AreEqual(CheckResultCode.EanInvalidLength, res.ResultCode);
+
+            res = httpClient.Post(_tooShortEan);
+            Assert.AreEqual(CheckResultCode.EanInvalidLength, res.ResultCode);
+
+            res = httpClient.Post(new EanEicCode(null));
+            Assert.AreEqual(CheckResultCode.NoCodeSupplied, res.ResultCode);
+
+            res = httpClient.Post(_nonDigitEan);
+            Assert.AreEqual(CheckResultCode.EanInvalidCharacter, res.ResultCode);
+
+            res = httpClient.Post(_invalidSumcheckEan);
+            Assert.AreEqual(CheckResultCode.EanInvalidCheckCharacter, res.ResultCode);
         }
     }
 }
